b12f91c98785cff6068e7c6bcd2b85b3a7cc8057
Shreyas Savanoor <shreyas@recruiterflow.com>
2020-03-13 23:55:13 +0530

    home page done with responsiveness

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+    selector: 'app-footer',
+    templateUrl: './footer.component.html',
+    styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    mobileView = false;
+
+    ngOnInit() {
+        this.resize();
+    }
+
+    resize() {
+        if (window.innerWidth < 768) {
+            this.mobileView = true;
+        } else {
+            this.mobileView = false;
+        }
+    }
 
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -9,25 +9,57 @@ export class HeaderComponent implements OnInit {
     headersList = [
         {
             label: 'HOME',
-            navigate: ''
+            navigate: '',
+            icon: 'fa fa-home'
         },
         {
             label: 'ABOUT',
-            navigate: '/about'
+            navigate: '/about',
+            icon: 'fa fa-info-circle'
         },
         {
             label: 'PROJECTS',
-            navigate: '/projects'
+            navigate: '/projects',
+            icon: 'fa fa-tasks'
         },
         {
             label: 'CONTACT',
-            navigate: '/contact'
+            navigate: '/contact',
+            icon: 'fa fa-address-card'
         }
     ]; 
 
+    socialProfiles = [
+        {
+            label: 'LinkedIn',
+            link: 'https://www.linkedin.com/in/shreyas-savanoor-b9911211a/',
+            icon: 'fa fa-linkedin'
+        },
+        {
+            label: 'Instagram',
+            link: 'https://www.instagram.com/shreyas_savanoor/',
+            icon: 'fa fa-instagram'
+        },
+        {
+            label: 'Facebook',
+            link: 'https://www.facebook.com/shreyas.savanoor',
+            icon: 'fa fa-facebook'
+        }
+    ];
+
+    mobileView = false;
     constructor() { }
 
     ngOnInit(): void {
+        this.resize();
+    }
+
+    resize() {
+        if(window.innerWidth < 768) {
+            this.mobileView = true;
+        } else {
+            this.mobileView = false;
+        }
     }
     
 }
