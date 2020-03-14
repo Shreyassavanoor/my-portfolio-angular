28492dff869f27fb5e5e9a3b45b5fdc1a410258e
Shreyas Savanoor <shreyas@recruiterflow.com>
2020-03-14 17:44:50 +0530

    about section done

--- src/app/features/about/about.component.ts
+++ src/app/features/about/about.component.ts
@@ -1,15 +1,63 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+    selector: 'app-about',
+    templateUrl: './about.component.html',
+    styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+    myBioLists = [
+        {
+            text: "Hi, I am Shreyas Savanoor Ravindra. Welcome to my world!!"
+        },
+        {
+            text: "I am pursuing master's in Computer Science at University of North Carolina at Charlotte."
+        },
+        {
+            text: "I have 33 months of industry experience working as Software Development Engineer. In my tenure of 33 months, I have worked in both MNC and an early stage start-up."
+        },
+        {
+            text: "I am interested to work in area's like Data Science, Machine Learning, Computer Vision, Image Processing and Python Developer. I love to build solutions for challenging problem's."
+        },
+        {
+            text: "My goal is to become jack of all and master of everything. My ultimate ambition in life is to become a very powerful person in tech industry."
+        }
+    ];
+    
+    mySkills = [
+        {
+            text: 'Python'
+        },
+        {
+            text: 'SciKit-Learn'
+        },
+        {
+            text: 'TensorFlow'
+        },
+        {
+            text: 'Angular'
+        },
+        {
+            text: 'React'
+        },
+        {
+            text: 'JavaScript'
+        }
+    ]
+    mobileView = false;
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.resize();
+    }
+
+    resize() {
+        if(window.innerWidth < 768) {
+            this.mobileView = true;
+        } else {
+            this.mobileView = false;
+        }
+    }
 
 }
--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,15 +1,60 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+    typewriter_text = ["I'm Looking for Internship Opportunities.", "I Love to Develop."];
+    index = 0;
+    typewriter_display: string = "";
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.typingCallback();
+    }
 
+    typingCallback() {
+        let array_length = this.typewriter_text.length;
+        if (this.index < array_length) {
+            this.showTypingText(this.typewriter_text[this.index]);
+        } else {
+            this.index = 0;
+            setTimeout(() => {
+                this.typingCallback();
+            }, 200);
+        }
+    }
+
+    showTypingText(text) {
+        let total_length = text.length;
+        let current_length = this.typewriter_display.length;
+        if (current_length < total_length) {
+            this.typewriter_display += text[current_length];
+            setTimeout(() => {
+                this.showTypingText(text);
+            }, 200);
+        } else {
+            setTimeout(() => {
+                this.clearTypingText(this.typewriter_display);
+            }, 200);
+        }
+    }
+
+    clearTypingText(text) {
+        let current_length = text.length;
+        if (current_length === 0) {
+            this.index += 1;
+            setTimeout(() => {
+                this.typingCallback();
+            }, 200);
+        } else {
+            this.typewriter_display = this.typewriter_display.slice(0, current_length - 1);
+            setTimeout(() => {
+                this.clearTypingText(this.typewriter_display);
+            }, 200);
+        }
+    }
 }
