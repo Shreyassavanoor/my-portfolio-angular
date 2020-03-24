e1509b10f377d97cf3d73dec26fe7a20a85a5591
Shreyas Savanoor <shreyas@recruiterflow.com>
2020-03-24 23:59:28 +0530

    adding resume is done

--- src/app/services/common-service.service.ts
+++ src/app/services/common-service.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Title } from '@angular/platform-browser';
+import * as firebase from "firebase/app";
+import 'firebase/storage';
+import { FIREBASECONFIG } from '../services/firebase/api-key';
 
 @Injectable({
     providedIn: 'root'
 })
 export class CommonServiceService {
     api: any = 'https://shreyassavanoor-portfolio.firebaseio.com/';
+    fileApi: any = 'https://shreyassavanoor-portfolio.appspot.com';
     constructor(private http: HttpClient, private titleService: Title) { }
 
     scroll(title) {
@@ -20,5 +24,23 @@ export class CommonServiceService {
 
     updateData(name, data) {
         return this.http.put(this.api + name + '.json', data);
+    }
+
+    getFile() {
+        // Initialize Firebase if it not initalized
+        if (!firebase.apps.length) {
+            firebase.initializeApp(FIREBASECONFIG.firebaseConfig);
+        }
+        
+        // Get a reference to the storage service, which is used to create references in your storage bucket
+        var storage = firebase.storage();
+
+        // Create a storage reference from our storage service
+        var storageRef = storage.ref();
+
+        // Create a child reference
+        var resume = storageRef.child('Shreyas_SR_Resume.pdf');
+
+        return resume.getDownloadURL();
     }
 }
